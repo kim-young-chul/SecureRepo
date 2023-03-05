4ed89baf5b090cd2b53406bdcad4e40c30cb04d0
kim-young-chul <126945988+kim-young-chul@users.noreply.github.com>
2023-03-06 04:04:23 +0900

    index.js login_success.js double quote to escape literal.

--- SecureCodingPrj/src/main/webapp/resources/js/index.js
+++ SecureCodingPrj/src/main/webapp/resources/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    let domain = $("#reurl").val();
+    let domain = $(`#reurl`).val();
     let address = domain + "/servlet/user_login";
     window.location.replace(address);
 })
