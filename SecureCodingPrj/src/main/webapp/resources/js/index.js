681f51110adc80ce8d868b1100cbaf0fe84a7ca1
kim-young-chul <126945988+kim-young-chul@users.noreply.github.com>
2023-03-07 19:58:14 +0900

    find selector append

--- SecureCodingPrj/src/main/webapp/resources/js/index.js
+++ SecureCodingPrj/src/main/webapp/resources/js/index.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-    window.location.replace($("input").attr("value") + "/servlet/user_login");
+    window.location.replace($("body").find("input").attr("value") + "/servlet/user_login");
 })
